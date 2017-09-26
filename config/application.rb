3e2f6c658e9b1992c3772a0a013574f35261d2af
Puneet <puneetgupta18190@gmail.com>
2017-09-26 06:25:24 +0000

    fc

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module RailsGuide
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = 'Europe/Warsaw'
+	# optional - note it can be only :utc or :local (default is :utc)
+	config.active_record.default_timezone = :local
+	platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
+	Bundler.require(platform)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
